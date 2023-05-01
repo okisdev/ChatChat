effb6821951ae2e64ab57facb4e3aeae5d7107b8
Harry Yep <github@hode.co.uk>
2023-05-01 22:30:31 +0100

    fix: meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,18 +28,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <meta name='msapplication-config' content='/icons/browserconfig.xml' />
                 <meta name='msapplication-TileColor' content='#2B5797' />
                 <meta name='msapplication-tap-highlight' content='no' />
-                <meta name='theme-color' content='#000000' />
+                <meta name='theme-color' content='#eee' />
 
-                <link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
-                <link rel='apple-touch-icon' sizes='152x152' href='/icons/touch-icon-ipad.png' />
-                <link rel='apple-touch-icon' sizes='180x180' href='/icons/touch-icon-iphone-retina.png' />
-                <link rel='apple-touch-icon' sizes='167x167' href='/icons/touch-icon-ipad-retina.png' />
-
+                <link rel='manifest' href='/manifest.json' />
+                <link rel='icon' type='image/png' href='/favicon.ico' />
+                <link rel='shortcut icon' type='image/png' href='/favicon.ico' />
+                <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
                 <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
                 <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-                <link rel='manifest' href='/manifest.json' />
                 <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#5bbad5' />
-                <link rel='shortcut icon' href='/favicon.ico' />
 
                 <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
                 <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
