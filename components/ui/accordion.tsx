421e38ddefc5445da3a04a4c99d2a9419fcdf887
Harry Yep <github@hode.co.uk>
2023-05-12 16:15:59 +0100

    ui: update @shadcn ui

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -33,7 +33,7 @@ const AccordionContent = React.forwardRef<React.ElementRef<typeof AccordionPrimi
     ({ className, children, ...props }, ref) => (
         <AccordionPrimitive.Content
             ref={ref}
-            className={cn('data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm transition-all', className)}
+            className={cn('overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down', className)}
             {...props}
         >
             <div className='pb-4 pt-0'>{children}</div>
