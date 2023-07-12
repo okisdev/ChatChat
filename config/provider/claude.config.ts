015eb81e1e7d3ffd9a4c6d13b9c5428ed9736d11
Harry Yep <github@hode.co.uk>
2023-07-12 12:31:01 +0800

    feat: claude new model

--- config/provider/claude.config.ts
+++ config/provider/claude.config.ts
@@ -1,14 +1,8 @@
 export const claudeModelConfig = [
     {
-        model: 'claude-v1',
+        model: 'claude-instant-1',
     },
     {
-        model: 'claude-v1-100k',
-    },
-    {
-        model: 'claude-instant-v1',
-    },
-    {
-        model: 'claude-instant-v1-100k',
+        model: 'claude-2',
     },
 ];
