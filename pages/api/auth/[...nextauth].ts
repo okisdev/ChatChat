0f81fa28cb11612000e2fdc576acd5b0886883d1
Amir Moradi <1281163+amirhmoradi@users.noreply.github.com>
2023-08-02 16:06:27 +0200

    revert debug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,4 @@ import NextAuth from 'next-auth';
 
 import { authOptions } from '@/lib/auth';
 
-if (process.env.NODE_ENV !== 'production'){
-  console.log(authOptions);
-}
 export default NextAuth(authOptions);
