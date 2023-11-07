7c0f12a1ed019f4b451b942c48d6e486fa62d2be
Harry Yep <git@hode.co.uk>
2023-11-07 10:47:59 +0800

    feat: openai new model

--- types/openai.d.ts
+++ types/openai.d.ts
@@ -1,4 +1,14 @@
-type OpenAIModel = 'gpt-4' | 'gpt-4-32k' | 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k';
+type OpenAIModel =
+    | 'gpt-4'
+    | 'gpt-4-0613'
+    | 'gpt-4-1106-preview'
+    | 'gpt-4-vision-preview'
+    | 'gpt-4-32k'
+    | 'gpt-4-32k-0613'
+    | 'gpt-3.5-turbo'
+    | 'gpt-3.5-turbo-16k'
+    | 'gpt-3.5-turbo-1106'
+    | 'gpt-3.5-turbo-instruct';
 type OpenAIMessageRole = 'user' | 'system' | 'assistant';
 
 interface OpenAIMessage {
