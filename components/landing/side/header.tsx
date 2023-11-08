baa7242da09f0cbc208191ac56f665e9ed5ec2be
Harry Yep <git@hode.co.uk>
2023-11-08 20:47:33 +0800

    fix: icon (#218)

--- components/landing/side/header.tsx
+++ components/landing/side/header.tsx
@@ -1,4 +1,4 @@
-'use client';
+import Image from 'next/image';
 
 import { useTranslations } from 'next-intl';
 
@@ -8,10 +8,11 @@ const SideHeader = () => {
     const t = useTranslations('');
 
     return (
-        <div className='flex items-center justify-between border-b px-1'>
+        <div className='flex items-center justify-start space-x-1 border-b px-2 py-1'>
+            <Image src='/OpenAI.svg' alt='logo' width={36} height={36} className='fill-black dark:fill-white' />
             <div className='p-1'>
                 <p className='gradient-flow bg-gradient-to-r bg-clip-text text-lg font-semibold leading-none text-transparent md:text-xl'>{siteConfig.title}</p>
-                <p className='text-xs font-medium'>{t(siteConfig.description)}</p>
+                <p className='text-xs font-normal'>{t(siteConfig.description)}</p>
             </div>
         </div>
     );
