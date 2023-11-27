6ac7523cd36c3958449bcc6e1ceb4879952699c7
Harry Yep <git@hode.co.uk>
2023-11-27 20:38:56 +0800

    feat: nextjs (#233)
    * feat: nextjs
    
    * fix: i18n

--- i18n.ts
+++ i18n.ts
@@ -2,4 +2,6 @@ import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ locale }) => ({
     messages: (await import(`./locales/${locale}.json`)).default,
+    timeZone: 'UTC',
+    now: new Date(),
 }));
