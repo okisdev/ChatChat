983f24cd78679bafadbb9d0a1cff10be17891a1b
Harry Yep <git@hode.co.uk>
2024-04-14 00:31:01 +0800

    fix: ai deps

--- lib/provider/Anthropic.ts
+++ lib/provider/Anthropic.ts
@@ -1,4 +1,4 @@
-import { Anthropic } from 'ai/anthropic';
+import { Anthropic } from '@ai-sdk/anthropic';
 
 export const anthropic = new Anthropic({
     apiKey: process.env.ANTHROPIC_API_KEY ?? '',
