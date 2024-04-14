717c009c8b36e1a41c28dbe0e33b50d82bb9cd0e
Harry Yep <git@hode.co.uk>
2024-04-14 14:11:52 +0800

    fix: hide share on search

--- app/[locale]/(home)/search/page.tsx
+++ app/[locale]/(home)/search/page.tsx
@@ -3,8 +3,7 @@
 import { AddButton } from '@/components/layout/add-button';
 import { ModelSelect } from '@/components/layout/model-select';
 import { SearchWindow } from '@/components/layout/search/search-window';
-import {SearchSelect} from '@/components/layout/search-select';
-import { ShareButton } from '@/components/layout/share-button';
+import { SearchSelect } from '@/components/layout/search-select';
 
 export const runtime = 'edge';
 
@@ -18,9 +17,6 @@ export default function Search() {
                     <AddButton />
                     <ModelSelect />
                     <SearchSelect />
-                </div>
-                <div className='flex items-center space-x-1 p-2'>
-                    <ShareButton />
                 </div>
             </div>
             <div className='container mx-auto w-full overflow-scroll md:w-10/12'>
