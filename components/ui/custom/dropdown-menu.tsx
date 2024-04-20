c7524652dfcf1ab0c184e1c9dd5b183f540b93cc
Harry Yep <git@hode.co.uk>
2024-04-21 00:33:08 +0800

    fix: dropdown style

--- components/ui/custom/dropdown-menu.tsx
+++ components/ui/custom/dropdown-menu.tsx
@@ -140,7 +140,7 @@ const DropdownMenuLabel = React.forwardRef<
 DropdownMenuLabel.displayName = DropdownMenuPrimitive.Label.displayName;
 
 const DropdownMenuSeparator = React.forwardRef<React.ElementRef<typeof DropdownMenuPrimitive.Separator>, React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>>(
-    ({ className, ...props }, ref) => <DropdownMenuPrimitive.Separator ref={ref} className={cn('-mx-1 my-1 h-px bg-muted dark:bg-neutral-400', className)} {...props} />
+    ({ className, ...props }, ref) => <DropdownMenuPrimitive.Separator ref={ref} className={cn('-mx-1 my-1 h-px bg-muted dark:bg-neutral-700', className)} {...props} />
 );
 DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName;
 
